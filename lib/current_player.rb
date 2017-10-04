301498aec056366b01c1edfe4318e4b6d8d283ab
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-04 18:17:30 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,38 @@
+def current_player(board)
+
+playerX = "X"
+playerO = "O"
+
+turns = turn_count(board)
+remainder = turns % 2
+
+if remainder == 0
+  playerX
+else
+if remainder == 1
+  playerO
+end
+end
+
+end
+
+def turn_count(board)
+
+count = 0
+
+
+
+board.each { |item|
+
+if item == " "
+
+else
+  #puts item
+  count += 1
+end
+   }
+
+count
+
+
+end
